41b0941b77915ae357a94ebde39e8c93d0e8fac8
Admutin <admutin@gmail.com>
2025-10-22 21:20:01 +0200

    feat: add sway

--- home.nix
+++ home.nix
@@ -136,5 +136,16 @@
     gtk3.extraCss = extraCss;
   };
 
-
+  wayland.windowManager.sway = {
+    enable = true;
+    config = rec {
+      modifier = "Mod4";
+      # Use kitty as default terminal
+      terminal = "kitty"; 
+      startup = [
+        # Launch Firefox on start
+        {command = "firefox";}
+      ];
+    };
+  };
 }
